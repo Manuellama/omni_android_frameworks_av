9a1c14fd5712c7ca685d99e62d3c428392a9a362
RyanG <decatf@gmail.com>
2014-11-09 14:14:24 -0500

    audioflinger: Threads: stream flush hack for ICS audio blobs
    Disable the stream flush command for Lollipop

--- services/audioflinger/Threads.cpp
+++ services/audioflinger/Threads.cpp
@@ -4243,8 +4243,10 @@ void AudioFlinger::DirectOutputThread::cacheParameters_l()
 
 void AudioFlinger::DirectOutputThread::flushHw_l()
 {
+#ifndef ICS_AUDIO_BLOB
     if (mOutput->stream->flush != NULL)
         mOutput->stream->flush(mOutput->stream);
+#endif
 }
 
 // ----------------------------------------------------------------------------
